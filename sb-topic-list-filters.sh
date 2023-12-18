eebfac29bdfd896b72a9d895affa59a87c4ebaa6
Darlan Oliveira <darlan.sistemas@gmail.com>
2023-12-18 20:22:39 -0300

    feat: :hammer: update filter rule extration

--- sb-topic-list-filters.sh
+++ sb-topic-list-filters.sh
@@ -16,18 +16,33 @@ for subscription in $(echo $subscriptions | jq -c '.[]'); do
 
     # Obter regras associadas à assinatura
     rules=$(az servicebus topic subscription rule list --resource-group $resourceGroup --namespace-name $sourceNamespace --topic-name $sourceTopic --subscription-name $subscriptionName)
-    echo $rule | jq .
+    # echo $rule | jq .
     # Iterar sobre regras e exibir detalhes
+   
     for rule in $(echo $rules | jq -c '.[]'); do
         ruleName=$(echo $rule | jq -r '.name')
         filterType=$(echo $rule | jq -r '.filterType')
         correlationFilter=$(echo $rule | jq -c '.correlationFilter')
         label=$(echo $correlationFilter | jq -r '.label')
-      
-
-        echo "   Regra: $ruleName"
-        echo "   Label do CorrelationFilter: $label"
-        echo "   Label do filterType: $filterType"
+        properties=$(echo $correlationFilter | jq -r '.properties')   
+        echo $properties
+        echo "Regra: $ruleName"
+        echo "Label do CorrelationFilter: $label"
+        echo "Label do filterType: $filterType"
+        sqlexpression=""
+        first=true
+        while IFS= read -r line; do
+            propertiesLabel=$(echo "$line" | cut -d ',' -f 1)
+            propertiesValue=$(echo "$line" | cut -d ',' -f 2)
+            
+            if [ "$first" = true ]; then
+                sqlexpression="$propertiesLabel='$propertiesValue'"
+                first=false
+            else
+                sqlexpression="$sqlexpression,$propertiesLabel='$propertiesValue'"
+            fi
+        done < <(echo "$properties" | jq -r 'to_entries[] |"\(.key),\(.value)"')
+        echo "SET $sqlexpression"
     done
 
     echo "---------------------------------------"
--- sb-topic-migration-v2.sh
+++ sb-topic-migration-v2.sh
@@ -15,29 +15,43 @@ subscriptions=$(az servicebus topic subscription list --resource-group $resource
 # 3. Iterar sobre Assinaturas e Criar no Novo Tópico
 for subscription in $(echo $subscriptions | jq -c '.[]'); do
     subscriptionName=$(echo $subscription | jq -r '.name')
-    ruler=$(echo $subscription | jq -r '.correlationFilter')
-    label=$(echo $subscription | jq -r '.correlationFilter.label')
-    ruleName=$(echo $subscription | jq -r '.correlationFilter.name')
-
+    
+    # Criar a assinatura no novo tópico
     az servicebus topic subscription create --resource-group $resourceGroup --namespace-name $destinationNamespace --topic-name $sourceTopic --name $subscriptionName
     
+    # Obter regras da assinatura do tópico de origem
     rules=$(az servicebus topic subscription rule list --resource-group $resourceGroup --namespace-name $sourceNamespace --topic-name $sourceTopic --subscription-name $subscriptionName)
-    
+
+    # Iterar sobre as regras e criar/atualizar no novo tópico
     for rule in $(echo $rules | jq -c '.[]'); do
         ruleName=$(echo $rule | jq -r '.name')
         filterType=$(echo $rule | jq -r '.filterType')
         correlationFilter=$(echo $rule | jq -c '.correlationFilter')
         label=$(echo $correlationFilter | jq -r '.label')
-        
+        properties=$(echo $correlationFilter | jq -r '.properties') 
 
-        az servicebus topic subscription rule create  \
-        --resource-group $resourceGroup \
-        --namespace-name $destinationNamespace \
-        --topic-name $sourceTopic \
-        --subscription-name $subscriptionName \
-        --name $ruleName \
-        --filter-type $filterType \
-        --label $label
+        correlationFilterProperties=()
+       
+        # Verificar se há propriedades
+        if [ "$(echo "$properties" | jq -c 'length')" -gt 0 ]; then
+            # Iterar sobre as propriedades e criar a lista de propriedades
+            while IFS= read -r line; do
+                propertiesLabel=$(echo "$line" | cut -d '=' -f 1)
+                propertiesValue=$(echo "$line" | cut -d '=' -f 2)
+                correlationFilterProperties+=("$propertiesLabel=$propertiesValue")
+            done < <(echo "$properties" | jq -r 'to_entries[] | "\(.key)=\(.value)"')
+        fi
+    
+        az servicebus topic subscription rule create \
+                --resource-group $resourceGroup \
+                --namespace-name $destinationNamespace \
+                --topic-name $sourceTopic \
+                --subscription-name $subscriptionName \
+                --name $ruleName \
+                --filter-type $filterType \
+                --label $label \
+                --correlation-filter $label \
+                --correlation-filter-property "${correlationFilterProperties[@]}"
     done
 done
 
